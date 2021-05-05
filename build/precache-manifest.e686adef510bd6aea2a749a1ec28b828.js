38c117d9ab45e0a48bc5d3fd89232f8e1149c7fe
vipvish <viplovevish13@gmail.com>
2021-05-05 19:40:05 +0530

    add build folder

---
+++ build/precache-manifest.e686adef510bd6aea2a749a1ec28b828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d54f1f365cdf84fe318231f750f4edf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27bdcf46f49dfa58de9",
+    "url": "/static/css/main.ce337672.chunk.css"
+  },
+  {
+    "revision": "409a1bfdbeeccc99300f",
+    "url": "/static/js/2.58d14947.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.58d14947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27bdcf46f49dfa58de9",
+    "url": "/static/js/main.3d3a7ebe.chunk.js"
+  },
+  {
+    "revision": "f6ca8bb2e593fd4a8e53",
+    "url": "/static/js/runtime-main.dce5d66a.js"
+  }
+]);
